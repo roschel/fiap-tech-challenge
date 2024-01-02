24d38480254bed34349bd342a291b7942c7ccfdc
Augusto <llaugustoll@gmai.com>
2024-01-02 07:55:24 -0300

    configurado conexão com banco por variavel de ambiente

--- tasty_delivery/settings.py
+++ tasty_delivery/settings.py
@@ -1,17 +1,18 @@
 from functools import lru_cache
-
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
+import os
 
 class Settings(BaseSettings):
     VERSION: str = Field("")
 
     # POSTGRES
-    DB_USERNAME: str = Field("postgres")
-    DB_PASSWORD: str = Field("password")
-    DB_HOST: str = Field("svc-tasty-db:5432")
-    DB_DATABASE: str = Field("tasty_delivery")
+
+    DB_USERNAME: str = Field(os.environ['DB_USERNAME'])
+    DB_PASSWORD: str = Field(os.environ["DB_PASSWORD"])
+    DB_HOST: str = Field(os.environ["DB_HOST"])
+    DB_DATABASE: str = Field(os.environ["DB_DATABASE"])
 
     # TOKEN
     ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(1440)
